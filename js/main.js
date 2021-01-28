90877581cc3e0859e56be12273f19433ec6eb1c5
Daniel Cefram Ramirez <dcefram@outlook.com>
2021-01-28 11:20:47 +0800

    init

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const familyName = document.querySelector('.family-name');
+const firstNames = document.querySelector('.first-names');
+const main = document.querySelector('main');
+
+familyName.addEventListener('animationend', () => {
+  firstNames.classList.add('animate');
+})
+
+firstNames.addEventListener('animationend', () => {
+  main.classList.add('animate');
+})
+
+let timeout = setTimeout(() => {
+  familyName.classList.add('animate');
+}, 5000);
+
+document.body.addEventListener('click', () => {
+  if (familyName.classList.contains('animate')) {
+    return;
+  }
+
+  clearTimeout(timeout);
+  timeout = setTimeout(() => {
+    familyName.classList.add('animate');
+  }, 1000);
+});
